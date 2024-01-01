291d85fd6810859635fcc83bca88d6b2763792bb
lucasLi920 <libin.lucas@foxmail.com>
2024-01-01 22:04:20 +0800

    WDOG interrupt and reset gen

---
+++ ip_repo/d_ip_wdog/rtl/wdog_flag_gen.sv
@@ -0,0 +1,10 @@
+module wdog_flag_gen #(
+  parameter   WDOG_CNT    = 16
+) (
+  input   logic     pclk,
+  input   logic     fclk,
+  output  logic     wdog_intrrupt,
+  output  logic     wdog_rst_flag
+);
+  
+endmodule
